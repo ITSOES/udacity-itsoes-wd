02b2f89263d44ddff0cf41c547a3bc3f6691cdbb
ITSOES <cronicmilk@yahoo.com>
2014-12-26 16:52:56 -0800

    Finished the first part of the signup page

--- unit4.py
+++ unit4.py
@@ -14,16 +14,23 @@ class Visits(Handler):
 
 
 EMAIL_RE = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
-
-
 def valid_email(email):
     return not email or EMAIL_RE.match(email)
 
+USERNAME_RE = re.compile(r'^[a-zA-Z0-9_-]{3,20}$')
+def invalid_username(un):
 
-class Member(db.Model):
+    if not (un or USERNAME_RE.match(un)):
+        return 'That\'s not a valid username!'
+    if Member.by_key(name=un):
+        return 'That username already exists!'
+
+class Member(Sitemodel):
     name = db.StringProperty(required=True)
     password_hash = db.StringProperty(required=True)
-    email = db.EmailProperty()
+    email = db.StringProperty(required=False)
+    # def testprint(self):
+    #     self.by_id('hello')
 
 
 #
@@ -44,39 +51,53 @@ class SignUp(Handler):
         self.renderSign()
 
     def post(self):
-        name = self.name = self.request.get('name')
+        username = self.name = self.request.get('username')
         password = self.password = self.request.get('password')
         verify = self.verify = self.request.get('verify')
         email = self.email = self.request.get('email')
 
-        if name and password and password == verify and valid_email(email):
-            self.setcookie('user', name)
-            self.redirect('/Unit4/Welcome/')
+        if not invalid_username(username) and password and password == verify and valid_email(email):
+            email = ''
+            self.setcookie('user', username)
+            self.saveUser(username, password, email)
+            self.redirect('/Unit4/welcome/')
 
-        params = dict(name=name,
+        params = dict(username=username,
                       email=email)
+        if invalid_username(username):
+            params['invalidusername'] = invalid_username(username)
         if not password:
-            params['invalidpassword'] = 'Password can\'t be empty'
+            params['invalidpassword'] = 'Password can\'t be empty!'
         if not valid_email(email):
             params['invalidemail'] = "This email is not a valid email."
         if password != verify:
-            params['invalidverify'] = 'Password must match'
+            params['invalidverify'] = 'Password must match!'
         self.renderSign(**params)
 
-    def saveUser(self, name, hash):
-        pass
+    def saveUser(self, name, password, email=''):
+        password_hash = self.hash_password(password)
+        User = Member(name=name, password_hash=password_hash, email=email)
+        User.put()
 
+class Login(SignUp):
+    def get(self):
+        self.renderSign('login.html')
+
+    def post(self):
+        pass
 
 class Welcome(SignUp):
     def get(self):
         user = self.readcookie('user')
         if not user:
-            self.redirect('/Unit4/SignUp/')
+            self.redirect('/Unit4/signup/')
         self.renderSign('welcome.html', name=user)
 
 
 app = webapp2.WSGIApplication([('/Unit4/Visits/?', Visits),
-                               ('/Unit4/SignUp/?', SignUp),
-                               ('/Unit4/Welcome/?', Welcome),
+                               ('/Unit4/signup/?', SignUp),
+                               ('/Unit4/welcome/?', Welcome),
+                               ('/Unit4/?', SignUp),
+                               ('/Unit4/login/?', Login),
                                ('.*', GoHome)  # Any junk urls goes to the homepage
                               ], debug=True)
